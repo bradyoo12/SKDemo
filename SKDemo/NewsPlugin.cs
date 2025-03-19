d8315b411b92d5ac83b19cc9c46c4702dc689020
bradyoo12 <bradyoo12@outlook.com>
2025-03-19 23:04:54 +0900

    Add project files.

---
+++ SKDemo/NewsPlugin.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SemanticKernel;
+using SimpleFeedReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKDemo
+{
+    public class NewsPlugin
+    {
+        [KernelFunction("get_news")]
+        [Description("Gets news items for today's date.")]
+        [return: Description("A list of current news stories.")]
+        public async Task<List<FeedItem>> GetNews(Kernel kernel, string category)
+        {
+            var reader = new FeedReader();
+            return (await reader.RetrieveFeedAsync($"https://rss.nytimes.com/services/xml/rss/nyt/{category}.xml"))
+                .Take(5)
+                .ToList();
+        }
+    }
+}
---
+++ SKDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using SKDemo;
+
+var builder = Kernel.CreateBuilder();
+
+// Services
+builder.AddAzureOpenAIChatCompletion("gpt-4o-mini",
+    "https://yooho-m8fynziw-eastus2.openai.azure.com/openai/deployments/gpt-4o-mini/chat/completions?api-version=2025-01-01-preview",
+    "CPZQx8rwqTmNk2c91rSyl8r68kPB9K70bXU4GcVaI1kkksn5ya9VJQQJ99BCACHYHv6XJ3w3AAAAACOGwXSo");
+
+// Plugins
+builder.Plugins.AddFromType<NewsPlugin>();
+
+var kernel = builder.Build();
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
+var chatMessages = new ChatHistory();
+
+while (true)
+{
+    Console.Write("Prompt:");
+    chatMessages.AddUserMessage(Console.ReadLine());
+
+    var completion = chatService.GetStreamingChatMessageContentsAsync(
+        chatMessages,
+        executionSettings: new OpenAIPromptExecutionSettings()
+        {
+            ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+        },
+        kernel: kernel);
+
+    var fullMessage = "";
+    await foreach (var content in completion)
+    {
+        Console.WriteLine(content.Content);
+        fullMessage += content.Content;
+    }
+
+    chatMessages.AddAssistantMessage(fullMessage);
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Hello, World!");
